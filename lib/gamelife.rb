6237a61eb4554bab7fb5f0e476c1bbf884d97cf1
Tania Munoz <tania.m.jimenez24@gmail.com>
2018-09-19 13:46:18 -0500

    files game

--- lib/gamelife.rb
+++ lib/gamelife.rb
@@ -1,5 +1,6 @@
-require "gamelife/version"
-
+require_relative "gamelife/version"
+require_relative "gamelife/rules"
+require_relative "gamelife/game"
 module Gamelife
   # Your code goes here...
 end
---
+++ main.rb
@@ -0,0 +1,5 @@
+require_relative "lib/gamelife"
+module Gamelife
+ game = Game_Life.new
+  game.start
+end
